3dd95eb0f24c5739c57df4b0031cb6b8f0813a40
lephuongnam97 <nam.lephuong@kyanon.digital>
2020-07-05 14:29:57 +0700

    First push

---
+++ app/Http/Controllers/Api/ProductController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Product;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $products = Product::all();
+
+        return $products->toJson();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        return Product::create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+        $product = Product::find($id);
+
+        return $product->toJson();
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+        $product = Product::find($id);
+        $product->name = $request->name;
+        $product->price = $request->price;
+        $product->active = $request->active;
+        $product->update();
+
+        return $product->toJson();
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+        $product = Product::find($id);
+        return $product->delete();
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+// Route::get('products','Api\ProductController@index')->name('products.index');
+// Route::get('products/{id}','Api\ProductController@show')->name('products.show');
+// Route::post('products','Api\ProductController@store')->name('products.store');
+// Route::put('products/{id}','Api\ProductController@update')->name('products.update');
+// Route::patch('products/{id}','Api\ProductController@update')->name('products.update');
+// Route::delete('products/{id}','Api\ProductController@destroy')->name('products.destroy');
+Route::apiResource('products','Api\ProductController');
