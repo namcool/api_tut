4c69f80bb6f82e8d943f72241bbd6cee778e48d0
lephuongnam97 <nam.lephuong@kyanon.digital>
2020-07-05 14:32:40 +0700

    Second push

--- routes/api.php
+++ routes/api.php
@@ -18,10 +18,10 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-// Route::get('products','Api\ProductController@index')->name('products.index');
-// Route::get('products/{id}','Api\ProductController@show')->name('products.show');
-// Route::post('products','Api\ProductController@store')->name('products.store');
-// Route::put('products/{id}','Api\ProductController@update')->name('products.update');
-// Route::patch('products/{id}','Api\ProductController@update')->name('products.update');
-// Route::delete('products/{id}','Api\ProductController@destroy')->name('products.destroy');
-Route::apiResource('products','Api\ProductController');
+Route::get('products','Api\ProductController@index')->name('products.index');
+Route::get('products/{id}','Api\ProductController@show')->name('products.show');
+Route::post('products','Api\ProductController@store')->name('products.store');
+Route::put('products/{id}','Api\ProductController@update')->name('products.update');
+Route::patch('products/{id}','Api\ProductController@update')->name('products.update');
+Route::delete('products/{id}','Api\ProductController@destroy')->name('products.destroy');
+// Route::apiResource('products','Api\ProductController');
